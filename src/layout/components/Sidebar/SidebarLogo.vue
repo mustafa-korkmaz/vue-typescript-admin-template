7196eaefdbbc534fdb0838be6f4bac2cbcc5dd4c
Mustafa Korkmaz <m.korkmaz@outlook.com>
2020-10-18 15:43:52 +0300

    add favicon.ico

--- src/layout/components/Sidebar/SidebarLogo.vue
+++ src/layout/components/Sidebar/SidebarLogo.vue
@@ -11,7 +11,7 @@
         to="/"
       >
         <img
-          src="favicon.svg"
+          src="favicon.ico"
           class="sidebar-logo"
         >
       </router-link>
@@ -22,7 +22,7 @@
         to="/"
       >
         <img
-          src="favicon.svg"
+          src="favicon.ico"
           class="sidebar-logo"
         >
         <h1 class="sidebar-title">
